5594975370197d4c65fa2f05c4859254a765ff5e
bassel27 <bassel_sabour@hotmail.com>
2024-12-03 21:05:27 +0200

    code modifications

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,41 +1,34 @@
-// src/components/Home/Home.tsx
 import React from 'react';
 import MovieCard from '../components/MovieCard';  // Import MovieCard component
 
-interface HomeProps {
-    movies: Array<any>; // Adjust the type as needed
-    searchTerm: string;
-    setSearchTerm: (term: string) => void;
-    searchMovies: (term: string) => void;
-}
-
-const Home: React.FC<HomeProps> = ({ movies, searchTerm, setSearchTerm, searchMovies }) => (
-    <div>
+function Home({ movies, searchTerm, setSearchTerm, searchMovies }){
+    return (
+      <div>
         <div className="search">
-            <input
-                placeholder="Search for Movies"
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-            />
-            <img
-                src="https://media.geeksforgeeks.org/wp-content/uploads/20230626112934/search.png"
-                alt="search icon"
-                onClick={() => searchMovies(searchTerm)}
-            />
+          <input
+            placeholder="Search for Movies"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <img
+            src="https://media.geeksforgeeks.org/wp-content/uploads/20230626112934/search.png"
+            alt="search icon"
+            onClick={() => searchMovies(searchTerm)}
+          />
         </div>
-
+  
         {movies?.length > 0 ? (
-            <div className="container">
-                {movies.map((movie) => (
-                    <MovieCard key={movie.imdbID} movie={movie} />
-                ))}
-            </div>
+          <div className="container">
+            {movies.map((movie) => (
+              <MovieCard key={movie.imdbID} movie={movie} />
+            ))}
+          </div>
         ) : (
-            <div className="empty">
-                <h2>No Movies found</h2>
-            </div>
+          <div className="empty">
+            <h2>No Movies found</h2>
+          </div>
         )}
-    </div>
-);
-
+      </div>
+    );
+  }
 export default Home;
